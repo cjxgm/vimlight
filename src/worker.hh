40dce0927edbff82e4bdee3db06d964078c434fb
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-08-03 15:44:06 +0800

    + add some comments on worker

--- src/worker.hh
+++ src/worker.hh
@@ -9,7 +9,13 @@ namespace vimlight
 		using command_func_type = vim::command_func_type;
 		using source_type = analyzer::source_type;
 
+		// should be called once and only once before calling any
+		// other functions in worker namespace.
+		//
+		// this function will run a worker thread in the background.
 		void run(command_func_type cmd);
+
+		// request the worker to highlight the source.
 		void request(const source_type& src);
 	};
 };
