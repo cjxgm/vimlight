e5e1363dc5b949c981b3c40ad91b04c6107eede3
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-08-03 15:12:32 +0800

    + library impl

---
+++ src/library.cc
@@ -0,0 +1,35 @@
+#include "library.hh"
+#include "worker.hh"
+
+static lua::library lib;
+static bool inited = false;
+
+namespace vimlight
+{
+	namespace library
+	{
+		int open(lua::state& s)
+		{
+			lib["init"] = [](lua::state& s) {
+				if (!inited) {
+					worker::command_func_type cmd;
+					s.get(cmd, 1);
+					worker::run(cmd);
+					inited = true;
+				}
+				return 0;
+			};
+
+			lib["request"] = [](lua::state& s) {
+				worker::source_type src;
+				s.get(src, 1);
+				worker::request(src);
+				return 0;
+			};
+
+			s.push(lib);
+			return 1;
+		}
+	};
+};
+
