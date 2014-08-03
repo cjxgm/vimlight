1c3ae310d034b02268847ca513427728a5645f98
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-08-03 16:32:44 +0800

    + thread communication done.

--- src/worker.cc
+++ src/worker.cc
@@ -1,4 +1,9 @@
 #include "worker.hh"
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <utility>
+
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -7,17 +12,53 @@ namespace vimlight
 {
 	namespace worker
 	{
+		namespace update
+		{
+			using mutex_type = std::mutex;
+			using condv_type = std::condition_variable;
+			using  lock_type = std::unique_lock<mutex_type>;
+			static mutex_type m;
+			static condv_type cv;
+			static bool request = false;
+			static source_type src;
+		};
+
 		void run(command_func_type cmd)
 		{
 			cout << ">>> worker::run " << reinterpret_cast<void*>(cmd) << endl;
+
+			std::thread th([cmd] {
+				source_type src;
+				while (true) {
+					{
+						update::lock_type lock(update::m);
+						update::cv.wait(lock, [] { return update::request; });
+						src = std::move(update::src);
+						update::request = false;
+					}
+					cout << ">>> worker::thread <<" << endl
+						<< src << endl
+						<< ">>" << endl;
+				}
+			});
+
+			th.detach();
 		}
 
-		void request(const source_type& src)
+		void request(source_type src)
 		{
 			cout << ">>> worker::request <<" << endl
 				<< src << endl
 				<< ">>" << endl;
+
+			{
+				update::lock_type lock(update::m);
+				update::src = std::move(src);
+				update::request = true;
+			}
+			update::cv.notify_one();
 		}
 	};
 };
+
 
