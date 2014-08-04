2a84eb9d11452f98bee276224bef91459b87bdc2
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-08-04 14:42:53 +0800

    @ worker: rework the worker interface

--- src/worker.hh
+++ src/worker.hh
@@ -1,22 +1,30 @@
 #pragma once
-#include "vim.hh"
 #include "analyzer.hh"
+#include "vim.hh"
+#include "highlight/type.hh"
 
 namespace vimlight
 {
 	namespace worker
 	{
-		using command_func_type = vim::command_func_type;
-		using source_type = analyzer::source_type;
+		using   source_type =  analyzer::  source_type;
+		using commands_type =       vim::commands_type;
+		using filename_type = highlight::filename_type;
 
 		// should be called once and only once before calling any
 		// other functions in worker namespace.
 		//
-		// this function will run a worker thread in the background.
-		void run(command_func_type cmd);
+		// this function will start a worker thread in the background.
+		void start(filename_type hlgroup);
 
 		// request the worker to highlight the source.
 		void request(source_type src);
+
+		// check if the last request is done
+		bool done();
+
+		// get the highlight result
+		commands_type get();
 	};
 };
 
