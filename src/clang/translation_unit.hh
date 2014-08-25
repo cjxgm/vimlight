0e13976cd6cae04355ee62508904490b491ee399
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-08-25 22:13:58 +0800

    + clang::translation_unit: support source renaming

--- src/clang/translation_unit.hh
+++ src/clang/translation_unit.hh
@@ -15,24 +15,31 @@ namespace clang
 		using self_type = translation_unit;
 		using super_type = internal::guard<c::translation_unit::type>;
 		using source_type = unsaved_file::source_type;
+		using filename_type = unsaved_file::name_type;
 		using index_type = clang::index;
 		using diagnostics_type = std::vector<clang::diagnostic>;
 
-		translation_unit(const index_type& index, const source_type& src="")
+		translation_unit(index_type& index) : index(index)
 		{
-			clang::unsaved_file f(src);
-			const char* argv[] = { "-std=gnu++1y" };
-			set(c::translation_unit::create_from_source_file(
-					index, f.name(), 1, argv, 1, f));
+			name("source.cc");
 		}
 
 		~translation_unit() override { if (owned) c::translation_unit::dispose(get()); }
 
+		void name(const filename_type& f)
+		{
+			clang::unsaved_file uf(f, "");
+			const char* argv[] = { "-std=gnu++1y" };
+			set(c::translation_unit::create_from_source_file(
+					index, f.c_str(), 1, argv, 1, uf));
+			file = f;
+		}
+
 		void parse(const source_type& src)
 		{
-			clang::unsaved_file f(src);
+			clang::unsaved_file uf(file, src);
 			auto opt = c::options::default_reparse(get());
-			c::translation_unit::reparse(get(), 1, f, opt);
+			c::translation_unit::reparse(get(), 1, uf, opt);
 		}
 
 		cursor cursor() const { return c::translation_unit::get_cursor(get()); }
@@ -46,6 +53,9 @@ namespace clang
 				diags.emplace_back(c::diagnostic::get(get(), i));
 			return std::move(diags);
 		}
+	private:
+		index_type& index;
+		filename_type file;
 	};
 };
 
