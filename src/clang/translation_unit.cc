ddfb2d383dda0dad23e48e34d9140f388ebe70fd
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-01-04 14:49:21 +0800

    + visualize

---
+++ src/clang/translation_unit.cc
@@ -0,0 +1,61 @@
+#include "translation_unit.hh"
+#ifdef LOG
+#include <fstream>
+using vimlight::log;
+
+static void replace(std::string& s,
+		std::string const& from, std::string const& to)
+{
+	using size_type = std::string::size_type;
+	for (size_type pos = 0;
+			(pos = s.find(from, pos)) != s.npos;
+			pos += to.size())
+		s.replace(pos, from.size(), to);
+}
+
+
+void visualize(std::ofstream & dot, clang::cursor c)
+{
+	if (!c.range().head().is_from_main()) return;
+
+	auto range = c.range();
+	auto head = range.head().position();
+	auto tail = range.tail().position();
+	auto loc  = c.location().position();
+	auto kind = c.kind().name();
+	auto name = c.name();
+	auto id = c.identifier();
+	replace(name, "\\", "\\\\");
+	replace(name, "\"", "\\\"");
+	dot << "\n\t\"" << id << "\" [label=\""
+		<< "[" << kind << "] \\\"" << name << "\\\"\\n"
+		<< head.y << ", " << head.x << " -> "
+		<< tail.y << ", " << tail.x << " @ "
+		<< loc.y << ", " << loc.x << "\"]\n";
+
+	c.each_child([&dot, &id](clang::cursor c) {
+		if (!c.range().head().is_from_main()) return false;
+		dot << "\t\"" << id << "\" -> \"" << c.identifier() << "\"\n";
+		return false;
+	});
+
+	c.each_child([&dot](clang::cursor c) {
+		visualize(dot, c);
+		return false;
+	});
+}
+
+void clang::translation_unit::visualize()
+{
+	std::ofstream dot{"/tmp/vimlight.dot"};
+	if (!dot) return;
+	dot << "digraph {\n"
+		<< "\tnode [fontname=\"Monospace\"]\n"
+		<< "\tnode [shape=box]\n"
+		<< "\tnode [width=0]\n";
+	::visualize(dot, cursor());
+	dot << "}\n\n";
+}
+
+#endif
+
