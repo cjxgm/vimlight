cbea5d674ee326670e6502a4723a6d2f8d1da4f5
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-11-12 12:59:48 +0800

    fix string ambiguity

--- src/clang/string.hh
+++ src/clang/string.hh
@@ -8,13 +8,11 @@ namespace clang
 	struct string : resource::unique<c::string::type>
 	{
 		using super_type = unique;
-		using cstring_type = char const*;
-		using  string_type = std::string;
+		using string_type = std::string;
 
 		string(value_type value) : super_type(value, c::string::dispose) {}
 
-		operator cstring_type() const { return c::string::cstr(get()); }
-		operator  string_type() const { return operator cstring_type(); }
+		operator  string_type() const { return c::string::cstr(get()); }
 	};
 }
 
